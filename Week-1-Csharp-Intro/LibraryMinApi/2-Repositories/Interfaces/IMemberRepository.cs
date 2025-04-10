656024f207da060b325de6db6c68eb1c9dc8a7fb
jdelacruz96 <jonathan.delacruz@revature.com>
2025-04-10 11:46:38 -0400

    added member repo and endpoint methods

---
+++ Week-1-Csharp-Intro/LibraryMinApi/2-Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+using Library.Models;
+
+namespace Library.Repositories;
+
+public interface IMemberRepository
+{
+    List<Member> GetAllMembers();
+
+    Member AddMember(Member memberToAdd);
+
+    void SaveMemberChanges(List<Member> members);
+
+}
--- Week-1-Csharp-Intro/LibraryMinApi/Program.cs
+++ Week-1-Csharp-Intro/LibraryMinApi/Program.cs
@@ -1,12 +1,14 @@
 //Up here, like any .cs file, we can throw in our using statements for packages or namespaces
 // that we may need
 using System.Text.Json;
+using Library.Models;
+using Library.Repositories;
 
 // Here is our builder
 var builder = WebApplication.CreateBuilder(args);
 
 //==== Dependency Injection Area ====
-
+builder.Services.AddSingleton<IMemberRepository, JsonMemberRepository>();
 
 // Here the builder takes all of our DI and middleware stuff and creates our app.
 var app = builder.Build();
@@ -15,6 +17,31 @@ var app = builder.Build();
 
 
 //Member endpoints
+app.MapGet("/members", (IMemberRepository repo) => {
+    try
+    {
+        return Results.Ok(repo.GetAllMembers());
+    }
+    catch (Exception ex)
+    {
+        // Using the Results prebuilt object to return the appropriate code
+        // if anything goes wrong
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapPost("/members", (Member memberToAdd, IMemberRepository repo) => {
+
+    try
+    {
+        return Results.Ok(repo.AddMember(memberToAdd));
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message, statusCode: 400); //We can return specific codes with Results.Problem if we want to
+    }
+
+});
 
 
 //Checkout endpoints
