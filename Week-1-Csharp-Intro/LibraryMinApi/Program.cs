10057e749b1339afb447e65b3f983483846c741f
jdelacruz96 <jonathan.delacruz@revature.com>
2025-04-10 12:10:28 -0400

    added api methods and swagger

--- Week-1-Csharp-Intro/LibraryMinApi/Program.cs
+++ Week-1-Csharp-Intro/LibraryMinApi/Program.cs
@@ -10,39 +10,64 @@ var builder = WebApplication.CreateBuilder(args);
 //==== Dependency Injection Area ====
 builder.Services.AddSingleton<IMemberRepository, JsonMemberRepository>();
 
+//Adding swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "LibraryAPI";
+    config.Title = "LibraryAPI";
+    config.Version = "v1";
+});
+
 // Here the builder takes all of our DI and middleware stuff and creates our app.
 var app = builder.Build();
+
+//Adding swagger
+if(app.Environment.IsDevelopment()) {
+    app.UseOpenApi();
+    app.UseSwaggerUi(config => {
+        config.DocumentTitle = "CarAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 //Book endpoints
 
 
 //Member endpoints
-app.MapGet("/members", (IMemberRepository repo) => {
-    try
+app.MapGet(
+    "/members",
+    (IMemberRepository repo) =>
     {
-        return Results.Ok(repo.GetAllMembers());
+        try
+        {
+            return Results.Ok(repo.GetAllMembers());
+        }
+        catch (Exception ex)
+        {
+            // Using the Results prebuilt object to return the appropriate code
+            // if anything goes wrong
+            return Results.Problem(ex.Message);
+        }
     }
-    catch (Exception ex)
+);
+
+app.MapPost(
+    "/members",
+    (Member memberToAdd, IMemberRepository repo) =>
     {
-        // Using the Results prebuilt object to return the appropriate code
-        // if anything goes wrong
-        return Results.Problem(ex.Message);
+        try
+        {
+            return Results.Ok(repo.AddMember(memberToAdd));
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message, statusCode: 400); //We can return specific codes with Results.Problem if we want to
+        }
     }
-});
-
-app.MapPost("/members", (Member memberToAdd, IMemberRepository repo) => {
-
-    try
-    {
-        return Results.Ok(repo.AddMember(memberToAdd));
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(ex.Message, statusCode: 400); //We can return specific codes with Results.Problem if we want to
-    }
-
-});
-
+);
 
 //Checkout endpoints
 
